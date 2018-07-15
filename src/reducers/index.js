271f398262e7f512f443ec8d9735ca740d1db8a8
psych0der <mayankbhola@gmail.com>
2018-07-15 23:40:51 +0530

    add userWidget reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import {
     combineReducers
 } from 'redux'
-import counter from './counter'
+import userWidget from './userWidget'
 
 export default combineReducers({
-    counter
+    userWidget
 })
---
+++ src/reducers/userWidget.js
@@ -0,0 +1,43 @@
+export const SET_NUMBER_OF_USERS = 'userwidget/SET_NUMBER_OF_USERS';
+export const SET_SORT_ORDER = 'userwidget/SET_SORT_ORDER';
+
+const initialState = {
+  userCount: 5,
+  sortOrder: 'asc',
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case SET_NUMBER_OF_USERS:
+      return {
+        ...state,
+        userCount: action.count,
+      };
+
+    case SET_SORT_ORDER:
+      return {
+        ...state,
+        sortOrder: action.sortOrder === 'dsc' ? 'dsc' : 'asc',
+      };
+
+
+    default:
+      return state;
+  }
+};
+
+// update user count
+export const setUserCount = count => (dispatch) => {
+  dispatch({
+    type: SET_NUMBER_OF_USERS,
+    count,
+  });
+};
+
+// set sorting order
+export const setSortOrder = sortOrder => (dispatch) => {
+  dispatch({
+    type: SET_SORT_ORDER,
+    sortOrder,
+  });
+};
