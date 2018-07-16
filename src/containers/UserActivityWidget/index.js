2ffbf148f00c4a2b1816a9b3b2cc3389b2f6b548
psych0der <mayankbhola@gmail.com>
2018-07-16 09:25:16 +0530

    remove header contents from user widget container

--- src/containers/UserActivityWidget/index.js
+++ src/containers/UserActivityWidget/index.js
@@ -1,44 +1,47 @@
 import React from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
 import memoize from 'memoize-one';
-import { setSortOrder, setUserCount } from '../../reducers/userWidget';
+import {setSortOrder, setUserCount} from '../../reducers/userWidget';
+import UserWidgetHeader from '../../components/UserWidgetHeader';
 import UserBlock from '../../components/UserBlock';
 import userData from './data.json';
-import downArrow from './icons/down.svg';
-import settingsLogo from './icons/settings.svg';
 import './index.css';
 
 class UserActivityWidget extends React.Component {
-    constructor(props) {
-        super(props)
-        this.normalizedData = userData;
-        this.denormalizedData = {users: []};
-    }
+  constructor(props) {
+    super(props)
+    this.normalizedData = userData;
+    this.denormalizedData = {
+      users: []
+    };
+  }
 
-    /**
+  /**
      * Use memoization to avoid expensive recomputes
      * Method to sort and slice the user list based on props
      * */
-    sortAndSlice = memoize((data, sortOrder, userCount) => {
-      /* user denormalized data as source */
-      let users = data.slice();
-      users.sort((a, b) => {
-        if (sortOrder === 'asc') {
-          return b.weekly - a.weekly;
-        }
-        return a.weekly - b.weekly;
-      });
-      /* slice users according to state */
-      users = users.slice(0, userCount);
-      return users;
+  sortAndSlice = memoize((data, sortOrder, userCount) => {
+    /* user denormalized data as source */
+    let users = data.slice();
+    users.sort((a, b) => {
+      if (sortOrder === 'asc') {
+        return b.weekly - a.weekly;
+      }
+      return a.weekly - b.weekly;
     });
+    /* slice users according to state */
+    users = users.slice(0, userCount);
+    return users;
+  });
 
-    componentDidMount() {
-      //  denormalized data
-      const data = this.normalizedData;
-      const users = [];
-      data.users.forEach((element) => {
+  componentDidMount() {
+    //  denormalized data
+    const data = this.normalizedData;
+    const users = [];
+    data
+      .users
+      .forEach((element) => {
         users.push({
           id: element.id,
           name: element.name,
@@ -46,80 +49,74 @@ class UserActivityWidget extends React.Component {
           fullname: `${element.name} ${element.lastname}`,
           daily: data.daily[element.id],
           weekly: data.weekly[element.id],
-          monthly: data.monthly[element.id],
+          monthly: data.monthly[element.id]
         });
       });
-      this.denormalizedData = {
-        users,
-      };
-      // trigger render after data is prepared
-      this.setState({});
-    }
+    this.denormalizedData = {
+      users
+    };
+    // trigger render after data is prepared
+    this.setState({});
+  }
 
-    setUserCount = (count) => {
-      this.props.setUserCount(count);
-    }
+  setUserCount = (count) => {
+    this
+      .props
+      .setUserCount(count);
+  }
 
-    setSortOrder = (sortOrder) => {
-      this.props.setSortOrder(sortOrder);
-    }
+  setSortOrder = (sortOrder) => {
+    this
+      .props
+      .setSortOrder(sortOrder);
+  }
 
-    render() {
-      let { sortOrder, userCount } = this.props;
-      /* sort users according to state */
-      let users = this.sortAndSlice(this.denormalizedData.users, sortOrder, userCount);
-      return (
+  clickDropDown = () => {}
+
+  render() {
+    let {sortOrder, userCount} = this.props;
+    /* sort users according to state */
+    let users = this.sortAndSlice(this.denormalizedData.users, sortOrder, userCount);
+    return (
+      <div>
+
         <div className="userActivityWidget">
-            <div className="widgetHeader">
-                <div className="titleText">
-                    Users activity
-                </div>
-                <div className="rightContainer">
-                    <div>
-                        <span className="activityDurationText">Weekly</span>
-                        <img src={downArrow} />
-
-                    </div>
-                    <div className="userWidgetSettings">
-                        <img src={settingsLogo} />
-                    </div>
-                </div>
-            </div>
-            <div className="userBoxes">
-                {users.map((item, i) => (
-                    <UserBlock
-                    userid={item.id}
-                    name={item.fullname}
-                    activity={item.weekly}
-                    key={item.id}
-                    />
-                ))}
-            </div>
+          <UserWidgetHeader/>
+          <div className="userBoxes">
+            {users.map((item, i) => (<UserBlock
+              userid={item.id}
+              name={item.fullname}
+              activity={item.weekly}
+              key={item.id}/>))}
+          </div>
 
           <div>
             <button onClick={() => this.setUserCount(3)}>
-                Make it 3
+              Make it 3
             </button>
             <button onClick={() => this.setUserCount(5)}>
-                Make it 3
+              Make it 3
             </button>
             <button onClick={() => this.setSortOrder('dsc')}>
-                DSC
+              DSC
             </button>
             <button onClick={() => this.setSortOrder('asc')}>
-                ASC
+              ASC
             </button>
           </div>
         </div>
-      );
-    }
+
+      </div>
+
+    );
+  }
 }
 
-const mapStateToProps = ({ userWidget }) => ({ userCount: userWidget.userCount, sortOrder: userWidget.sortOrder });
+const mapStateToProps = ({userWidget}) => ({userCount: userWidget.userCount, sortOrder: userWidget.sortOrder});
 
 const mapDispatchToProps = dispatch => bindActionCreators({
   setSortOrder,
-  setUserCount,
+  setUserCount
 }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserActivityWidget);
