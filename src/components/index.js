50635dec33379f32131e853b64149dda0266348c
psych0der <mayankbhola@gmail.com>
2018-07-18 04:16:32 +0530

    create index file for components

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export {default as DashboardHeader} from './DashboardHeader';
+export {default as UserBlock} from './UserBlock/';
+export {default as UserWidgetHeader} from './UserWidgetHeader';
+export {default as UserWidgetSettings} from "./UserWidgetSettings";
+export {default as UserWidgetSettingsHeader} from "./UserWidgetSettingsHeader";
+export {default as WidgetContainer}  from "./WidgetContainer";
+export {default as WidgetCreationModal} from "./WidgetCreationModal";
+export {default as UserActivityWidgetDescription} from "./UserActivityWidgetDescription";
