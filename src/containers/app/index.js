543cdcc1cd963b61688ea1adfc018a0d92f2682c
psych0der <mayankbhola@gmail.com>
2018-07-18 04:27:26 +0530

    fix references

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Route} from 'react-router-dom'
-import Home from '../home'
+import Home from '../Home'
 // import About from '../about'
 
 import {DragDropContext} from 'react-dnd';
